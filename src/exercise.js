5a951be647e2bd79af73fb90a5372a25e08779b7
Nicolas Molina Monroy <hi@nicobytes.com>
2022-09-02 19:28:59 -0400

    :construction: work

--- src/exercise.js
+++ src/exercise.js
@@ -1,4 +1,4 @@
-export function runCode() {
+export function fetchRetry(url, times, count = 1) {
   // Tu código aquí 👈
   // Para poder monitorear de la función fetch en las pruebas
   // Asegurate de usarla usando window.fetch
