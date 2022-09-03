8b7416490adecd8c4ee1fe62e163953492098d0f
Nicolas Molina Monroy <hi@nicobytes.com>
2022-09-02 20:05:50 -0400

    :construction: work
    gg

--- src/exercise.test.js
+++ src/exercise.test.js
@@ -1,35 +1,48 @@
 import { fetchRetry } from "./exercise";
 
-async function solution(url) {
-  const response = await fetch(url);
-  return response.json();
-}
-
 describe("tests", () => {
-
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it("should throw a error with 1 retry", () => {
-    expect(() => fetchRetry('https://domain-a.com/api-1', 1)).rejects.toThrowError(new Error("Invalid request with 1 retries"));
+    jest.spyOn(global, "fetch").mockRejectedValue("error");
+    expect(() =>
+      fetchRetry("https://domain-a.com/api-1", 1)
+    ).rejects.toThrowError(new Error("Invalid request with 1 retries"));
+    expect(() =>
+      fetchRetry("https://domain-a.com/api-1", 2)
+    ).rejects.toThrowError(new Error("Invalid request with 2 retries"));
+    expect(() =>
+      fetchRetry("https://domain-a.com/api-1", 3)
+    ).rejects.toThrowError(new Error("Invalid request with 3 retries"));
   });
 
-  it("should call fetch 2 times", () => {
-    const spy = jest.spyOn(global, 'fetch')
-    expect(() => fetchRetry('https://domain-a.com/api-1', 2)).rejects.toThrowError(new Error("Invalid request with 2 retries"));
+  it("should call fetch 2 times", async () => {
+    const spy = jest.spyOn(global, "fetch").mockRejectedValue("error");
+    try {
+      await fetchRetry("https://domain-a.com/api-1", 3);
+    } catch {}
     expect(spy).toHaveBeenCalledTimes(2);
   });
 
-  it("should call fetch 3 times", () => {
-    const spy = jest.spyOn(global, 'fetch')
-    expect(() => fetchRetry('https://domain-a.com/api-1', 3)).rejects.toThrowError(new Error("Invalid request with 3 retries"));
-    expect(spy).toHaveBeenCalledTimes(3);
+  it("should call fetch 3 times", async () => {
+    const spy = jest.spyOn(global, "fetch").mockRejectedValue("error");
+    try {
+      await fetchRetry("https://domain-a.com/api-1", 3);
+    } catch {}
+    expect(spy).toHaveBeenCalledTimes(2);
   });
 
   it("should return the data in json format", async () => {
-    const usersOriginal = await solution('https://jsonplaceholder.typicode.com/users');
-    const users = await fetchRetry('https://jsonplaceholder.typicode.com/users', 1);
-    expect(users).toEqual(usersOriginal);
+    const spy = jest
+      .spyOn(global, "fetch")
+      .mockResolvedValue({ json: () => Promise.resolve([1, 2, 3, 4]) });
+    const users = await fetchRetry(
+      "https://jsonplaceholder.typicode.com/users",
+      1
+    );
+    expect(users).toEqual([1, 2, 3, 4]);
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
